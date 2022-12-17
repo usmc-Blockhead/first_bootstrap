01a2aa4de0bd448fe7fb5bb0105b778402e97175
usmc-Blockhead <jaysplaypen@gmail.com>
2022-12-17 12:30:17 -0800

    week4 assignment

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(function() {
+    
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+
+    $("#reserveButton").click(function(){
+        $('#reserveModal').modal('toggle')
+    });
+
+    $("#loginButton").click(function(){
+        $('#loginModal').modal('toggle')
+    });
+
+
+    // document.getElementById('something').click();
+    
+
+    // $("p").click(function(){
+    //     $(this).hide();
+    // });
+
+
+
+});
